65def1abb3daf205e91e4a51f920270a0eb6b1ce
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-11-26 12:22:20 +0530

    Update PlacifyApplication.java

---
+++ src/main/java/com/jbk/PlacifyApplication.java
@@ -0,0 +1,11 @@
+package com.jbk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacifyApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PlacifyApplication.class, args);
+	}
+}
