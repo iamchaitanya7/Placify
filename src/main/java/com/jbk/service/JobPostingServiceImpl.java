40b2e78a798da7a4110619f429ae87e957a31427
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-11-28 16:56:05 +0530

    Update JobPostingServiceImpl.java

--- src/main/java/com/jbk/service/JobPostingServiceImpl.java
+++ src/main/java/com/jbk/service/JobPostingServiceImpl.java
@@ -1,4 +1,45 @@
 package com.jbk.service;
 
-public class JobPostingServiceImpl {
+import com.jbk.dao.JobPostingDao;
+import com.jbk.entities.JobPosting;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobPostingServiceImpl implements JobPostingService {
+
+    @Autowired
+    private JobPostingDao jobPostingDao;
+
+    @Override
+    public int createJob(JobPosting jobPosting) {
+        return jobPostingDao.saveJob(jobPosting);
+    }
+
+    @Override
+    public List<JobPosting> retrieveAllJobs() {
+        return jobPostingDao.getAllJobs();
+    }
+
+    @Override
+    public JobPosting retrieveJobById(int id) {
+        return jobPostingDao.getJobById(id);
+    }
+
+    @Override
+    public int deleteJob(int id) {
+        return jobPostingDao.deleteJobById(id);
+    }
+
+    @Override
+    public int updateJob(JobPosting jobPosting) {
+        return jobPostingDao.updateJob(jobPosting);
+    }
+
+    @Override
+    public int createMultipleJobs(List<JobPosting> jobPostings) {
+        return jobPostingDao.saveMultipleJobs(jobPostings);
+    }
 }
