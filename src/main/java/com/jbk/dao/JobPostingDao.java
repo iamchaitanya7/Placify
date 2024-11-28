d846cc7cdfd1d3cd47f22cf190236ef645d51acd
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-11-28 15:07:30 +0530

    Update JobPostingDao.java

--- src/main/java/com/jbk/dao/JobPostingDao.java
+++ src/main/java/com/jbk/dao/JobPostingDao.java
@@ -1,4 +1,13 @@
 package com.jbk.dao;
 
+import com.jbk.entities.JobPosting;
+import java.util.List;
+
 public interface JobPostingDao {
+    int saveJob(JobPosting jobPosting);
+    List<JobPosting> getAllJobs();
+    JobPosting getJobById(int id);
+    boolean deleteJobById(int id);
+    boolean updateJob(JobPosting jobPosting);
+    int saveMultipleJobs(List<JobPosting> jobPostings);
 }
