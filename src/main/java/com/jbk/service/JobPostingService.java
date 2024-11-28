049c6d67b994fb75eb2683a6108ce4daa87a8394
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-11-28 16:48:44 +0530

    Update JobPostingService.java

--- src/main/java/com/jbk/service/JobPostingService.java
+++ src/main/java/com/jbk/service/JobPostingService.java
@@ -1,4 +1,14 @@
 package com.jbk.service;
 
+import com.jbk.entities.JobPosting;
+
+import java.util.List;
+
 public interface JobPostingService {
+    int createJob(JobPosting jobPosting);
+    List<JobPosting> retrieveAllJobs();
+    JobPosting retrieveJobById(int id);
+    int deleteJob(int id);
+    int updateJob(JobPosting jobPosting);
+    int createMultipleJobs(List<JobPosting> jobPostings);
 }
